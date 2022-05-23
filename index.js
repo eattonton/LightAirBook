aa939c794bfe5853a2f3a3258db12b7348255703
eattonton <eattonton@qq.com>
2022-05-23 09:11:29 +0800

    5-23 A

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 datas.active = Vue.ref(0)
 datas.activeBotTag = Vue.ref(0)
 datas.lstAllBooks = []
-
+datas.bookCategorys = [
+        {thumb:"https://img.yzcdn.cn/public_files/2017/10/24/e5a5a02309a41f9f5def56684808d9ae.jpeg"},
+        {thumb:"https://img.yzcdn.cn/public_files/2017/10/24/1791ba14088f9c2be8c610d0a6cc0f93.jpeg"}]
 //定义响应式属性
 props.menus = []
 props.lstBooks = []
 
 const mounted = () => {
 
-    model.menus(null, (res) => {
+    model.menus({category:'教辅资料'}, (res) => {
         if (res && res["items"]) {
             res["items"].forEach((item, index) => {
                 //1.加载标签
-                props.menus.push({ name: item.name })
+                props.menus.push({label:item.name.substring(item.name.indexOf('#')+1), name: item.name })
                 //2.加载书
                 addBook(index,item["children"]);
             });
