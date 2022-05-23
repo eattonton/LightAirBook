809510653b2d32e8eb0fb06f83c3150f87fdb2e0
eattonton <eattonton@qq.com>
2022-05-24 07:55:10 +0800

    5-24 A

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
-const APP_VERSION = "1.0.4";
+const APP_VERSION = "1.0.5";
 //const APP_BASE_URL ="http://localhost:8088/";
 //const APP_BASE_URL ="http://localhost:50264/";
-const APP_BASE_URL ="https://triweb.cn/";
+//const APP_BASE_URL ="https://triweb.cn/";
+const APP_BASE_URL ="http://dady.triweb.cn/";
